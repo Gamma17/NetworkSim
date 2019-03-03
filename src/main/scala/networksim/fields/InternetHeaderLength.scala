80c55965a538fa4c3a38574fbadff4b429df84db
Gamma17 <pclerkin@fastmail.fm>
2019-03-03 20:49:19 +0000

    Add field validation logic

--- src/main/scala/networksim/fields/InternetHeaderLength.scala
+++ src/main/scala/networksim/fields/InternetHeaderLength.scala
@@ -3,7 +3,5 @@ package networksim.fields
 class InternetHeaderLength(ihl: Int) {
   val min: Int = 5
   val max: Int = (scala.math.pow(2, 4) - 1).intValue
-  if (ihl < min || ihl > max) {
-    throw new IllegalArgumentException("IHL must be in range %d to %d, inclusive".format(min, max))
-  }
+  require(ihl < min || ihl > max, "IHL must be in range %d to %d, inclusive".format(min, max))
 }
--- src/main/scala/networksim/fields/Version.scala
+++ src/main/scala/networksim/fields/Version.scala
@@ -1,9 +1,16 @@
 package networksim.fields
 
-class Version(version: Int = 4) {
-  val min: Int = 0
-  val max: Int = (scala.math.pow(2, 4) - 1).intValue
-  if (version < min || version > max) {
-    throw new IllegalArgumentException("Version must be in range %d to %d, inclusive".format(min, max))
-  }
+object Version {
+  val Name = "Version"
+  val Min = 0
+  val Max: Int = (scala.math.pow(2, 4) - 1).intValue
+  val Ipv4 = 4
+  val supportedValues = Array(Ipv4)
+}
+
+class Version(value: Int = 4) {
+
+  import Version._
+
+  FieldValidator.validate(Name, Min, Max, supportedValues, value)
 }

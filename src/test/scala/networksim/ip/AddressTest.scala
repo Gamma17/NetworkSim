1d0cfe34fadfeec07eb6f0efca498c61b78403ae
Gamma17 <pclerkin@fastmail.fm>
2019-03-15 15:24:49 +0000

    Formatting

--- src/test/scala/networksim/ip/AddressTest.scala
+++ src/test/scala/networksim/ip/AddressTest.scala
@@ -7,7 +7,7 @@ class AddressTest extends FunSuite {
   test("IP addresses are correctly represented") {
     val address = Address("166.20.17.255")
     assert(address.octets == (166, 20, 17, 255))
-    assert(address.value ==  2786333183L)
+    assert(address.value == 2786333183L)
   }
 
   test("Invalid IP addresses are rejected") {
